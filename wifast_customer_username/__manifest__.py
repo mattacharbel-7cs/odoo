2981d9d257f4531ebfed16683f25e94192d48770
Dow Group <dowgeorges10@gmail.com>
2026-07-06 16:50:07 +0300

    Add files via upload

---
+++ wifast_customer_username/__manifest__.py
@@ -0,0 +1,17 @@
+{
+    'name': 'WiFast Customer Username',
+    'version': '19.0.1.0.0',
+    'summary': 'Adds a WiFi/customer account username to contacts with a secured API.',
+    'author': 'Dow Group',
+    'website': 'https://www.dowgroup.com',
+    'category': 'Contacts',
+    'license': 'LGPL-3',
+    'depends': ['contacts'],
+    'data': [
+        'views/res_partner_views.xml',
+        'views/res_config_settings_views.xml',
+    ],
+    'installable': True,
+    'auto_install': False,
+    'application': False,
+}
